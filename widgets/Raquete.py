c595b68f21735dce5b0a6da7388080da670f1f5f
yagoas <yago.amin@gmail.com>
2020-11-18 15:20:41 -0300

    Refatoração

--- widgets/Raquete.py
+++ widgets/Raquete.py
@@ -20,9 +20,12 @@ class Raquete(Widget):
     # Define a colisão da raquete com a bola
     def rebate_bola(self, bola):
 
+        # Calculo das distâncias entre os widgets para definir se houve colisão
         dx = self.center_x - bola.center_x
         dy = self.center_y - bola.center_y
         distancia = math.hypot(dx, dy)
+
+        # Defini velocidade ganha pela bola (obs: achar formula para considerar "força" da colisão e usar para acc)
         acc = 3
 
         # Verifica se houve a colisão do widget "raquete" com o widget "bola"
@@ -32,13 +35,17 @@ class Raquete(Widget):
             if self.sound_disco_raquete:
                 self.sound_disco_raquete.play()
             
+            # Seta velocidade da bola em 0 evitar bugs
             bola.velocidade = 0, 0
+
+            # Pega tulpa da posição da bola na hora da colisão
             bx, by = bola.pos
-            rx, ry = self.pos
-            teste_x = (bx - rx)/(math.pi**2) + bx
-            teste_y = (by - ry)/(math.pi**2) + by
 
-            vetor_x = (bx - teste_x)*acc
-            vetor_y = (by - teste_y)*acc
+            # Define um vetor resultante dados as posições e distancias no momento da colisão
+            razao_x = (-dx)/(math.pi**2) + bx
+            razao_y = (-dy)/(math.pi**2) + by
+            vetor_x = (bx - razao_x)*acc
+            vetor_y = (by - razao_y)*acc
 
+            # Seta a velcidade da bola resultante da colisão
             bola.velocidade = (-vetor_x) , (-vetor_y) 
